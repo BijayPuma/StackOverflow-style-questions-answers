6f9f257fd966b85ca0c63473b4d2ec7703703b0e
Bijay Rai <raibjraibj@yahoo.com>
2018-11-15 01:00:41 -0500

    added crud but not working fk

--- index.js
+++ index.js
@@ -5,20 +5,21 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-//body parser middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
 mongoose.Promise = global.Promise;
 
 //connect to Mongoose
 mongoose
-  .connect("mongodb://localhost/questionsAnswers")
+  .connect(
+    "mongodb://localhost/questionsAnswers",
+    {
+      useMongoClient: true
+    }
+  )
   .then(() => console.log("MongoDB Connected.."))
   .catch(err => console.log(err));
 
 //Load Question Model
-require("./models/questions");
+require("./models/Questions");
 const Question = mongoose.model("questions");
 
 //Handlebars middleware
@@ -28,38 +29,80 @@ app.engine(
     defaultLayout: "main"
   })
 );
+
 app.set("view engine", "handlebars");
 
+//body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Index Route
 app.get("/", (req, res) => {
-  res.render("index");
+  const title = "Welcome";
+
+  res.render("index", {
+    title: title
+  });
 });
+
+//About Route
 app.get("/about", (req, res) => {
   res.render("about");
 });
 
 //Questions index Page where your question will be posted
-app.get("/questions/index", (req, res) => {
-  Question.find({})
-    .sort({ date: "desc" })
-    .then(questions => {
-      res.render("./questions/index", {
-        questions: questions
-      });
+app.get("/questions", (req, res) => {
+  Question.find({}).then(questions => {
+    res.render("./questions/index", {
+      questions: questions
     });
+  });
 });
 
 //Add Question Form
 app.get("/questions/addquestions", (req, res) => {
-  res.render("./questions/addquestions");
+  res.render("questions/addquestions");
 });
 
-//Post Process Form
+//Edit Question Form
+app.get("/questions/edit/:id", (req, res) => {
+  Question.findOne({
+    _id: req.params.id
+  }).then(question => {
+    res.render("./questions/edit", {
+      quesiton: question
+    });
+  });
+});
+
+//Process Form
 app.post("/questions", (req, res) => {
   Question.create({
     title: req.body.title,
     question: req.body.question
   }).then(ideas => {
     res.redirect("./questions/index");
+  });
+});
+
+//Edit Form Process
+app.put("/questions/:id", (req, res) => {
+  Question.findOne({
+    _id: req.params.id
+  }).then(idea => {
+    //new values
+    question.title = req.body.title;
+    question.question = req.body.question;
+    question.save().then(question => {
+      res.redirect("/questions");
+    });
+  });
+});
+
+//Delete Question
+app.delete("/questions/:id", (req, res) => {
+  Question.remove({ _id: req.params.id }).then(() => {
+    res.redirect("/questions");
   });
 });
 
